d6bde69088fe2ba22dd1d25a9fa760336e40ce1f
dhruvashastri12 <dhruvashastri12@gmail.com>
2016-07-03 16:52:25 +0530

    overflow menu - settings string update

--- app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
+++ app/src/main/java/com/example/navigationdrawerdemo/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.navigationdrawerdemo;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("MyAction", null).show();
             }
         });
 
